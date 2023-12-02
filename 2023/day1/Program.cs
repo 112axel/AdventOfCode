45c7a71bc85c7dcfae06628c4483a84a305e4750
Axel Cullbrand <axel.cullbrand@gmail.com>
2023-12-02 15:38:56 +0100

    day 1 done

---
+++ 2023/day1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.RegularExpressions;
+
+var input = File.ReadAllLines("./input.txt");
+
+var sum = 0;
+
+string[] strings = {"zero","one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+foreach (var line in input)
+{
+    var matches = Regex.Matches(line, @"(?=(\d|one|two|three|four|five|six|seven|eight|nine))").Select(x => x.Groups[1].Value);
+    var filterLine = "";
+
+    filterLine = String.Concat(matches.Select(match => match.Length != 1? Array.IndexOf(strings, match).ToString():match.ToString()));
+
+
+    var first = filterLine.First(x => char.IsDigit(x));
+    var last = filterLine.Last(x => char.IsDigit(x));
+    var combined = first.ToString()+ last.ToString();
+    Console.WriteLine($"combined {combined}");
+    sum += int.Parse(combined);
+}
+Console.WriteLine(sum.ToString());
+
+
+
+
+
+//foreach (var line in input)
+//{
+//    var first = line.First(x => char.IsDigit(x));
+//    var last = line.Last(x => char.IsDigit(x));
+//    var combined = first.ToString()+ last.ToString();
+//    Console.WriteLine($"combined {combined}");
+//    sum += int.Parse(combined);
+//}
+//Console.WriteLine(sum.ToString());
