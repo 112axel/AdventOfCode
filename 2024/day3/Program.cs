d702214f6620d4bfc239f664958c01aef5f593fa
Axel Cullbrand <axel.cullbrand@gmail.com>
2024-12-03 20:50:37 +0100

    Part 1

---
+++ 2024/day3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+
+var input = File.ReadAllText("./input.txt");
+
+
+
+Regex regex = new Regex(@"mul\((\d+),(\d+)\)");
+
+Regex removeDont = new Regex(@"don't\(\).+?do\(\)");
+
+
+var a = regex.Matches(input);
+
+
+int result = 0;
+foreach (Match match in a)
+{
+    result += (int.Parse(match.Groups[1].ToString()) * int.Parse(match.Groups[2].ToString()));
+}
+
+Console.WriteLine(result);
